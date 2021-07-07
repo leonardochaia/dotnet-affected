3eaab0963a0d0960471341724ee155601799afef
Leonardo Chaia <lchaia@outlook.com>
2021-07-07 15:34:43 -0700

    chore: add SDK 3 as well

--- eng/install-sdk.sh
+++ eng/install-sdk.sh
@@ -14,3 +14,4 @@ global_json_file="$(dirname "$0")/../global.json"
 dotnet_install_dir="$(dirname "$0")/.dotnet"
 
 "$install_script" --install-dir "$dotnet_install_dir" --jsonfile "$global_json_file"
+"$install_script" --install-dir "$dotnet_install_dir" --channel 3.0
