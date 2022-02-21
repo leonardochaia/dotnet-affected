820ab5d812b0763bd06f80fee1192ce0138fbd9d
Stano Peťko <stano@stanopetko.eu>
2022-02-21 20:50:15 +0100

    Add Powershell scripts

---
+++ eng/install-sdk.ps1
@@ -0,0 +1,13 @@
+# Downloads the dotnet-install.ps1 script and executes it against the global.json file.
+
+$installScriptUrl = "https://dot.net/v1/dotnet-install.ps1"
+$installScript = "$PSScriptRoot\dotnet-install.ps1"
+
+Write-Host "Downloading '$installScriptUrl'"
+Invoke-WebRequest -Uri $installScriptUrl -OutFile $installScript -MaximumRetryCount 10 -RetryIntervalSec 3
+
+$globalJsonFile = "$PSScriptRoot\..\global.json"
+$dotnetInstallDir = "$PSScriptRoot\.dotnet"
+
+. $installScript  -InstallDir $dotnetInstallDir -JSonFile $globalJsonFile
+. $installScript  -InstallDir $dotnetInstallDir -Channel 3.1
