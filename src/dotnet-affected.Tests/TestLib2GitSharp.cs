c691d3a414c40e70ee804149578a78d1c861dd6f
Leonardo Chaia <leonardochaia@protonmail.com>
2022-03-01 12:00:48 -0300

    chore: print out macos paths

---
+++ src/dotnet-affected.Tests/TestLib2GitSharp.cs
@@ -0,0 +1,34 @@
+﻿using LibGit2Sharp;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Affected.Cli.Tests
+{
+    public class TestLib2GitSharp
+    {
+        private readonly ITestOutputHelper _helper;
+
+        public TestLib2GitSharp(ITestOutputHelper helper)
+        {
+            _helper = helper;
+        }
+
+        [Fact]
+        public void TestRepoPaths()
+        {
+            _helper.WriteLine($"OS: {Environment.OSVersion}");
+            using var directory = new TempWorkingDirectory();
+
+            _helper.WriteLine($"Request create repo at {directory.Path}");
+
+            var createdRepoPath = Repository.Init(directory.Path);
+            _helper.WriteLine($"Output Git Init {createdRepoPath}");
+            
+            using var repo = new Repository(directory.Path);
+
+            _helper.WriteLine($"Repo Path: {repo.Info.Path}");
+            _helper.WriteLine($"Repo Workdir: {repo.Info.WorkingDirectory}");
+        }
+    }
+}
