6abf0fae660e55be2301ce47d876b3dc62007df2
Niklas Krebs <niki.krebs@web.de>
2023-03-15 13:14:32 +0100

    removed unused import

--- src/dotnet-affected/Extensions/LinqExtensions.cs
+++ src/dotnet-affected/Extensions/LinqExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 
 namespace Affected.Cli.Extensions
@@ -15,7 +14,7 @@ namespace Affected.Cli.Extensions
         /// Filters out projects where the project name matches the <paramref name="regexPattern"/> provided.
         /// <param name="source">The Enumerable to filter in</param>
         /// <param name="propertySelector">The property to filter</param>
-        /// <param name="regexPattern">The regex pattern to match for.</param>
+        /// <param name="regexPattern">The regex pattern to match for</param>
         /// </summary>
         public static IEnumerable<T> RegexExclude<T>(this IEnumerable<T> source, Func<T,string> propertySelector, string regexPattern)
         {
