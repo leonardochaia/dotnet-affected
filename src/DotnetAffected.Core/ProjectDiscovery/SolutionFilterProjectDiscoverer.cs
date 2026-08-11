23410777cfd47d7f4a5c21315734f112f678f5d1
Leonardo Chaia <leonardochaia@protonmail.com>
2026-08-11 08:07:07 -0300

    chore: use the new SolutionFilter to simplify slnf input
    Co-authored-by: Gunnar Liljas <gunnar.liljas@gmail.com>

---
+++ src/DotnetAffected.Core/ProjectDiscovery/SolutionFilterProjectDiscoverer.cs
@@ -0,0 +1,21 @@
+﻿using DotnetAffected.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace DotnetAffected.Core
+{
+    /// <summary>
+    /// Discovers projects from a Solution Filter file (.slnf).
+    /// </summary>
+    internal class SolutionFilterProjectDiscoverer : IProjectDiscoverer
+    {
+        public IEnumerable<string> DiscoverProjects(IDiscoveryOptions options)
+        {
+            // It should not be possible for this to be null based on call paths,
+            // but this makes the warning go away.
+            ArgumentNullException.ThrowIfNull(options.FilterFilePath);
+
+            return SolutionFilter.Load(options.FilterFilePath).ProjectPaths;
+        }
+    }
+}
